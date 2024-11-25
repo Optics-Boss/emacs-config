97aca908439f1e3ef46e87b9f31d24b9a87a8466
Optics-Boss <->
2024-11-25 21:39:21 +0100

    Started working on the config

--- init.el
+++ init.el
@@ -2,4 +2,10 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+(set-background-color "ivory")
 (setq inhibit-startup-screen t)
+
+(progn
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+)
